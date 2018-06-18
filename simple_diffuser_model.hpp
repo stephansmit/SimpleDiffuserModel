93b5997b54c6308086dd7cecd4f6d68b11f7ed1f
stephan <stephansmit@hotmail.com>
2018-06-18 09:47:47 +0200

    ordered

--- simple_diffuser_model.hpp
+++ simple_diffuser_model.hpp
@@ -4,40 +4,37 @@
 class SimpleDiffuserModel {
 
 protected:
-		CFluidModel *StateInlet;
-		CFluidModel *StateOutlet;
-		CFluidModel *StateIsentropicOutlet;
-		su2double VelocityInlet, VelocityOutlet;
-		void CalculateOutletState(double A_in,double A_out, double h0_in, double c_in, double P_out);
+	CFluidModel *StateInlet;
+	CFluidModel *StateOutlet;
+	CFluidModel *StateIsentropicOutlet;
+	su2double VelocityInlet, VelocityOutlet;
+	void CalculateOutletState(double A_in,double A_out, double h0_in, double c_in, double P_out);
 
 public:
-		SimpleDiffuserModel(void);
-		SimpleDiffuserModel(double P_in,
-							double rho_in,
-							double h0_in,
-							double c_in,
-							double A_in,
-							double A_out,
-							double P_out
-							);
+	SimpleDiffuserModel(void);
+	SimpleDiffuserModel(double P_in,
+						double rho_in,
+						double h0_in,
+						double c_in,
+						double A_in,
+						double A_out,
+						double P_out
+						);
 
-		su2double GetEnthalpyOut();
-		su2double GetDensityOut();
-		su2double GetVelocityOut();
-		su2double GetPressureOut();
+	su2double GetEnthalpyOut();
+	su2double GetDensityOut();
+	su2double GetVelocityOut();
+	su2double GetPressureOut();
+	su2double GetIsentropicEnthalpyOut();
+	su2double GetIsentropicDensityOut();
+	su2double GetIsentropicPressureOut();
+	su2double GetEnthalpyIn();
+	su2double GetDensityIn();
+	su2double GetVelocityIn();
+	su2double GetPressureIn();
+	su2double GetEfficiency();
 
-		su2double GetIsentropicEnthalpyOut();
-		su2double GetIsentropicDensityOut();
-		su2double GetIsentropicPressureOut();
-
-		su2double GetEnthalpyIn();
-		su2double GetDensityIn();
-		su2double GetVelocityIn();
-		su2double GetPressureIn();
-
-		su2double GetEfficiency();
-
-		virtual ~SimpleDiffuserModel(void);
+	virtual ~SimpleDiffuserModel(void);
 
 };
 
@@ -55,13 +52,17 @@ SimpleDiffuserModel::SimpleDiffuserModel(double P_in,
 	su2double Pstar = 4126300.0;        //critical pressure toluene
 	su2double Tstar = 591.75;           //critical temperature toluene
 	su2double w = 0.2657;
+
+	//inlet
 	StateInlet = new CPengRobinson(gamma, R, Pstar, Tstar, w);
 	StateInlet->SetTDState_Prho(P_in, rho_in);
 	VelocityInlet = c_in;
 
+	//isentropic outlet
 	StateIsentropicOutlet = new CPengRobinson(gamma, R, Pstar, Tstar, w);
 	StateIsentropicOutlet->SetTDState_Ps(P_out, StateInlet->GetEntropy());
 
+	//outlet
 	StateOutlet = new CPengRobinson(gamma, R, Pstar, Tstar, w);
 	CalculateOutletState(A_in, A_out, h0_in, c_in, P_out);
 
